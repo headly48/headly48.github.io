36348c8bea285c81e45038b6f04ceaa2ba93e425
Joshua Head <joshua.head48@gmail.com>
2016-08-01 23:33:43 +1100

    Blog update at 2016-08-01 12:33:43

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,55 +2,58 @@
  * Main JS file for Casper behaviours
  */
 
-/* globals jQuery, document */
-(function ($, undefined) {
+/*globals jQuery, document */
+(function ($) {
     "use strict";
 
-    var $document = $(document);
+    $(document).ready(function(){
 
-    $document.ready(function () {
+        $(".postcontent_typesome").fitVids();
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+        function casperFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".postcontent_typesome").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
 
-        $(".scroll-down").arctic_scroll();
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        };
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
+        casperFullImg();
+        $(window).smartresize(casperFullImg);
 
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+}(jQuery));
 
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+(function($,sr){
 
-        allOptions = $.extend(defaults, options);
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
 
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
 
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
 
-    };
-})(jQuery);
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
